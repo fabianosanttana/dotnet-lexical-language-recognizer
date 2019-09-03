5acea73f29ac4cb74b5849e9d4e122dd5f4d6104
Fabiano Santana <fabianosanttana@uds.com.br>
2019-09-02 21:23:56 -0300

    Alteração do arquivo e inserção de comentário

--- Utils/TokenLexeme.cs
+++ Utils/TokenLexeme.cs
@@ -5,6 +5,11 @@ namespace AFD
         private object descart;
         private string v;
 
+        /// <summary>
+        /// Método construtor para lexema
+        /// </summary>
+        /// <param name="descart">Token</param>
+        /// <param name="v">Palavra do arquivo</param>
         public TokenLexeme(object descart, string v)
         {
             this.descart = descart;
